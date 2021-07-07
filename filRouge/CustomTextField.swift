b5c9d1e9ccd8809dd83e3868343c096cfaa46b6d
Student04 <student04@TFNS5304.local>
2021-07-07 16:50:36 +0200

    login

---
+++ filRouge/CustomTextField.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTextField.swift
+//  filRouge
+//
+//  Created by Student04 on 07/07/2021.
+//
+
+import Foundation
---
+++ filRouge/UIColor.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor.swift
+//  filRouge
+//
+//  Created by Student04 on 07/07/2021.
+//
+
+import Foundation
